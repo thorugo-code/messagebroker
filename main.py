13014dc4760442bf605d1ec2d633d779d9e52bb2
Vitor Hugo Lima <vitorhrflima@gmail.com>
2024-03-08 10:07:37 -0300

    feat: possibility to use s3 bucket to save received data

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from models import *
 
 if __name__ == "__main__":
 
+    start = time.time()
+
     try:
         with open('config.json', 'r') as file:
             data = json.load(file)
@@ -32,8 +34,17 @@ if __name__ == "__main__":
         raise KeyError('File config.json must have user, password, endpoint, vhost and queue keys.')
 
     try:
-        receive(endpoint, vhost, queue, user, password)
+        """
+        If you want to save the data to an S3 bucket, set the bucket name here
+        Else, leave it as None by default and the data will be saved to a local file
+        Example: s3_bucket = 'your-bucket-name'
+        """
+
+        s3_bucket = None
+        receive(endpoint, vhost, queue, user, password, bucket=s3_bucket)
     except KeyboardInterrupt:
         print('Exiting...')
     except Exception as e:
         print(f'An error occurred: ({type(e)}) {e}. Exiting...')
+
+    print(f"Execution time: {round(time.time() - start, 2)} seconds")
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
+import boto3
+import time
 import json
 import pika
 import ssl
@@ -46,22 +48,39 @@ def send(endpoint, vhost, queue, data_to_send, username, password):
     connection.close()
 
 
-def write_json(new_data, filename='data.json'):
-    try:
-        with open(filename, 'r+') as file:
-            file_data = json.load(file)
-            file_data["received_data"].append(new_data)
-            file.seek(0)
-            json.dump(file_data, file, indent=4)
+def write_json(new_data, s3_client=None, s3_bucket=None, object_key='data.json'):
+    if s3_bucket is not None:
+        try:
+            existing_data = {}
+            try:
+                response = s3_client.get_object(Bucket=s3_bucket, Key=object_key)
+                existing_data = json.loads(response['Body'].read().decode('utf-8'))
+            except s3_client.exceptions.NoSuchKey:
+                pass
 
-    except FileNotFoundError:
-        with open(filename, 'w') as file:
-            data = dict()
-            data['received_data'] = [new_data]
-            json.dump(data, file, indent=4)
+            existing_data.setdefault("received_data", []).append(new_data)
+
+            s3_client.put_object(
+                Bucket=s3_bucket,
+                Key=object_key,
+                Body=json.dumps(existing_data, indent=4).encode('utf-8')
+            )
+        except Exception as e:
+            print(f"Error writing JSON data to S3: {e}")
+    else:
+        try:
+            with open('data.json', 'r') as file:
+                data = json.load(file)
+                data.setdefault("received_data", []).append(new_data)
+
+            with open('data.json', 'w') as file:
+                json.dump(data, file, indent=4)
+        except FileNotFoundError:
+            with open('data.json', 'w') as file:
+                json.dump({"received_data": [new_data]}, file, indent=4)
 
 
-def receive(endpoint, vhost, queue, username, password):
+def receive(endpoint, vhost, queue, username, password, bucket=None):
     host, port = amqps(endpoint)
 
     connection = pika.BlockingConnection(
@@ -74,7 +93,14 @@ def receive(endpoint, vhost, queue, username, password):
     channel = connection.channel()
 
     def callback(ch, method, properties, body):
-        write_json(json.loads(body.decode('utf-8')))
+        data = json.loads(body.decode('utf-8'))
+
+        write_json(
+            new_data=data,
+            s3_client=boto3.client('s3') if bucket else None,
+            s3_bucket=bucket
+        )
+
         print(f"\t[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Received '{(body.decode('utf-8'))[:20]}"
               f"{'...' if len((body.decode('utf-8'))) > 20 else ''}' at '/{vhost}/{queue}'")
 
